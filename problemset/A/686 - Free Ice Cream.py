12d28b6f491334e226b6ee90a5b5d2e76e8b45c2
Khalid Elmesallamy <khalidalmosallamy@gmail.com>
2020-09-13 16:56:42 +0300

    686 - Free Ice Cream.py

---
+++ problemset/A/686 - Free Ice Cream.py
@@ -0,0 +1,16 @@
+people, packs = input().split()
+people = int(people)
+packs = int(packs)
+distress = 0
+
+for i in range(people):
+    action, pack = input().split()
+    if action == "+":
+        packs += int(pack)
+    elif action == "-" and packs >= int(pack):
+        packs -= int(pack)
+    elif action == "-" and packs < int(pack):
+        distress += 1
+
+
+print(f"{packs} {distress}", end="")
