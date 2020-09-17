1b1c2804f381da091a9abf184cb6a272130bf7d8
Khalid Elmesallamy <khalidalmosallamy@gmail.com>
2020-09-17 07:35:36 +0300

    567 - Lineland Mail

--- problemset/A/567 - Lineland Mail.py
+++ problemset/A/567 - Lineland Mail.py
@@ -1,15 +1,9 @@
 num = int(input())
 cities = [int(i) for i in input().split()]
-
-def length(x1,x2):
-    return abs(x1 - x2)
-
+print(f"{abs(cities[0] - cities[1])} {abs(cities[0] - cities[num-1])}")
 for index, city in enumerate(cities):
-    if index == 0:
-        print(f"{length(city, cities[1])} {length(city, cities[num-1])}")
-    elif index == num -1:
-        print(f"{length(city, cities[index - 1])} {length(city, cities[0])}", end="")
-    else:
-        minBetween = min(length(city, cities[index - 1]), length(city, cities[index + 1]))
-        minEnd = max(length(city, cities[0]), length(city, cities[num - 1]))
+    if index != (num - 1) and index != 0:
+        minBetween = min(abs(city - cities[index - 1]), abs(city - cities[index + 1]))
+        minEnd = max(abs(city - cities[0]), abs(city - cities[num - 1]))
         print(f"{min(minBetween, minEnd)} {max(minBetween, minEnd)}")
+print(f"{abs(cities[num - 1] - cities[num - 2])} {abs(cities[num - 1] - cities[0])}", end="")
